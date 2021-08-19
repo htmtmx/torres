f295864026080c0761b8d77fe9359d21c99b3a77
Christan RCSG <christian.floppy@gmail.com>
2021-08-18 19:30:55 -0500

    Vista - Cliente Detalles

--- home/detalles-cliente.php
+++ home/detalles-cliente.php
@@ -25,20 +25,18 @@ $idCliente=$_GET['idCliente'];
             <div class="header-body">
                 <div class="row align-items-center py-4">
                     <div class="col-lg-6 col-auto">
-                        <h6 class="h2 text-white d-inline-block mb-0">Inicio</h6>
                         <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                             <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                                 <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
                                 <li class="breadcrumb-item"><a href="./">Inicio</a></li>
                                 <li class="breadcrumb-item"><a href="./clientes.php">Clientes</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Detalles del cliente</li>
-
                             </ol>
                         </nav>
                     </div>
                     <div class="col-lg-6 col-auto text-right">
-                        <a href="#" class="btn btn-sm btn-neutral">Registrar Adquisicion</a>
-                        <a href="#" class="btn btn-sm btn-neutral">Nueva Venta</a>
+                        <a href="#!" class="btn btn-sm btn-warning">Supender</a>
+                        <a href="#!" class="btn btn-sm btn-danger">Eliminar</a>
                     </div>
                 </div>
             </div>
@@ -46,18 +44,103 @@ $idCliente=$_GET['idCliente'];
     </div>
     <!-- Page content -->
     <div class="container-fluid mt--6">
-
-
+        <div class="row">
+            <div class="col-xl-12 order-xl-1">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">NOMBRE DEL CLIENTE</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <h6 class="heading-small text-muted mb-4">Información del Cliente</h6>
+                            <div class="pl-lg-4">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-username">Username</label>
+                                            <input type="text" id="input-username" class="form-control" placeholder="Username" value="lucky.jesse">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-email">Email address</label>
+                                            <input type="email" id="input-email" class="form-control" placeholder="jesse@example.com">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-first-name">First name</label>
+                                            <input type="text" id="input-first-name" class="form-control" placeholder="First name" value="Lucky">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-last-name">Last name</label>
+                                            <input type="text" id="input-last-name" class="form-control" placeholder="Last name" value="Jesse">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="my-4" />
+                            <!-- Address -->
+                            <h6 class="heading-small text-muted mb-4">Datos de Empresa</h6>
+                            <div class="pl-lg-4">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-address">Address</label>
+                                            <input id="input-address" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-city">City</label>
+                                            <input type="text" id="input-city" class="form-control" placeholder="City" value="New York">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-country">Country</label>
+                                            <input type="text" id="input-country" class="form-control" placeholder="Country" value="United States">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-country">Postal code</label>
+                                            <input type="number" id="input-postal-code" class="form-control" placeholder="Postal code">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="my-4" />
+                            <!-- Description -->
+                            <div class="col-lg-12 col-auto text-right">
+                                <button type="submit" class="btn btn-primary">Guardar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-xl-12">
                 <div class="card">
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col">
-                                <h3 class="mb-0">Table Demo</h3>
+                                <h3 class="mb-0">Direcciones</h3>
                             </div>
                             <div class="col text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">Ver Todos</a>
+                                <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#addCliente">
+                                    <i class="fas fa-plus"></i> Nueva Dirección</a>
                             </div>
                         </div>
                     </div>
@@ -66,91 +149,17 @@ $idCliente=$_GET['idCliente'];
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                             <tr>
-                                <th scope="col">No Contrado</th>
+                                <th scope="col">#</th>
+                                <th scope="col">No</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Contacto</th>
                                 <th scope="col">Detalles</th>
-                                <th scope="col">Fecha Venta</th>
-                                <th scope="col">General</th>
-                                <th scope="col">Avance de Pagos</th>
+                                <th scope="col">Registro</th>
                                 <th scope="col"></th>
                             </tr>
                             </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">
-                                    156156
-                                </th>
-                                <td>
-                                    Nissan Versa 2015
-                                </td>
-                                <td>
-                                    15 de Enero de 2021
-                                </td>
-                                <td>
-                                    <i class="fas fa-calendar-check text-green"></i> Al corriente
-                                </td>
-                                <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">60%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="dropdown show">
-                                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Acciones
-                                        </a>
+                            <tbody id="tbl-clientes">
 
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                            <a class="dropdown-item" href="#"><i class="fas fa-eye text-blue"></i> Ver Contrato</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-hand-holding-usd text-green"></i> Abonar</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-cloud-upload-alt text-orange"></i> Subir Archivo</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-times text-red"></i> Cancelar</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    156156
-                                </th>
-                                <td>
-                                    Nissan Versa 2015
-                                </td>
-                                <td>
-                                    15 de Enero de 2021
-                                </td>
-                                <td>
-                                    <i class="fas fa-calendar-times text-red"></i> Atrasado
-                                </td>
-                                <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">60%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="dropdown show">
-                                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Acciones
-                                        </a>
-
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                            <a class="dropdown-item" href="#"><i class="fas fa-eye text-blue"></i> Ver Contrato</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-hand-holding-usd text-green"></i> Abonar</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-cloud-upload-alt text-orange"></i> Subir Archivo</a>
-                                            <a class="dropdown-item" href="#"><i class="fas fa-times text-red"></i> Cancelar</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
                             </tbody>
                         </table>
                     </div>
